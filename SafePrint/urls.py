fb196c050cbe8871b17a51ee420fe4c918615079
vincentperezzz <perezvincent100@gmail.com>
2025-06-04 14:22:35 +0800

    Add initial Django project structure with base view and URL routing

--- SafePrint/urls.py
+++ SafePrint/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from main.views import base_view 
 
 urlpatterns = [
+    path('', base_view, name='home'), 
     path('admin/', admin.site.urls),
+    path('base/', base_view, name='base'),
 ]
