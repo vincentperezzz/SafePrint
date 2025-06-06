80e42d7759c1cb8ade1148c013e7f85a8c3860ae
vincentperezzz <perezvincent100@gmail.com>
2025-06-06 14:44:09 +0800

    Add upload view and template; update navigation links for improved routing

--- SafePrint/urls.py
+++ SafePrint/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path
-from main.views import index_view 
+from main.views import index_view, upload_view
 
 urlpatterns = [
     path('', index_view, name='home'), 
     path('admin/', admin.site.urls),
     path('index/', index_view, name='index'),
+    path('upload/', upload_view, name='upload'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def index_view(request):
     return render(request, 'index.html')
+
+def upload_view(request):
+    return render(request, 'upload.html')
