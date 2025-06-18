1a2e428fc00d76be56ce9b868bea2347c402c1d9
Crystal <datucrystaline@gmail.com>
2025-06-19 01:08:11 +0800

    Add feedback URL pattern to portal URLs for feedback submissions

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
@@ -7,4 +7,5 @@ urlpatterns = [
     path('upload/', views.upload_view, name='upload'),
     path('confirmation/', views.confirmation, name='confirmation'),
     path('login/', views.login_view, name='login'),
+    path('', include('portal.urls')),
 ]
