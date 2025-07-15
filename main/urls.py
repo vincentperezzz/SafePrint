ba20a15577780404ac6329173283ec6dbfc29718
vincentperezzz <perezvincent100@gmail.com>
2025-07-15 20:31:42 +0800

    Refactor URL patterns to serve static files in DEBUG mode; remove unused service file

--- main/urls.py
+++ main/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -17,3 +19,6 @@ urlpatterns = [
     path('api/update-document-settings/', views.update_document_settings, name='update_document_settings'),
     path('', include('portal.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
