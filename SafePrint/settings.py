b01ac9c1614ab7e529982e30739a9e18fbd312a9
Server <perezvincent100@gmail.com>
2026-02-08 01:52:34 +0000

    feat: Update domain references from 'nanoprint' to 'safeprint' in configuration files and scripts

--- SafePrint/settings.py
+++ SafePrint/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY', default='default-secret-key')
 # DEBUG = config('DEBUG', default=False, cast=bool)
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.100', 'safeprint', 'nanoprint.duckdns.org']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '192.168.0.100', 'safeprint', 'safeprint.duckdns.org']
 
 # CSRF settings for HTTPS
 CSRF_TRUSTED_ORIGINS = []
